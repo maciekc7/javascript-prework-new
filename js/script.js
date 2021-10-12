7c0f199d692ad372dc93e87cfe531b00e0cacdc1
maciekc7 <ciulkinmaciek7@gmail.com>
2021-10-12 22:51:56 +0200

    add var randomNumber & var myRandomNumber

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier'
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
+
+/// org randomNumber ///
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
+
+/// Zmień powyższy kod tak, aby dodatkowo losował liczbę całkowitą od 11 do 19. //
+
+var myRandomNumber;
+myRandomNumber = Math.floor(Math.random() * 9 + 1 + 10);
+printMessage('Wylosowana liczba z przedziału od 11 do 19 to: ' + myRandomNumber);
+
