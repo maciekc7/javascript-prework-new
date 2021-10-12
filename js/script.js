1532ce317b70e1457af9cded376e738a18f2e538
maciekc7 <ciulkinmaciek7@gmail.com>
2021-10-12 23:11:37 +0200

    add player movement rules

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@
 
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
+console.log('wylosowana liczba to: ' + randomNumber);11
 
 if (randomNumber == '1') {
   computerMove = 'kamień';
@@ -47,6 +47,24 @@ if (randomNumber == '1') {
 } else if (randomNumber == '3') {
   computerMove = 'nożyce';
 }
-printMessage('Mój ruch: ' + computerMove);
+printMessage('Komputera ruch: ' + computerMove);
+
+/// new part 
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
 
 
+1
+if (playerInput == '1') {
+  playerMove = 'kamień';
+  playerMove = 'papier';
+} else if (playerInput == '3'){
+    playerMove = 'nożyce';
+} else {
+    playerMove = 'kamień';
+    printMessage('Błędny wybór: Domyślnie wybrano: kamień')
+}
+
+printMessage('Twój ruch: ' + playerMove);
